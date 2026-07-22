99efd369ecdf284b056d5583ffea4680daa2f2f4
Oava <ortiqbekwerov@gmail.com>
2026-07-22 15:09:46 +0500

    I added html minifier and auto prifexer

--- quiz-app/vite.config.js
+++ quiz-app/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
   base: './',
+  plugins: [
+    ViteMinifyPlugin({}),
+  ],
 })
